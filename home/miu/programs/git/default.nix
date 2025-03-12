dcb74c3db2893bde764091ebe6a4a9c91177a09e
ms16183 <ms21826@outlook.com>
2025-03-12 09:31:06 +0900

    [add] git command

--- home/miu/programs/git/default.nix
+++ home/miu/programs/git/default.nix
@@ -6,8 +6,10 @@
     userEmail = "ms21826@outlook.com";
     userName = "ms16183";
     aliases = {
+      d = "diff";
       s = "status";
       l = "log --graph --oneline";
+      g = "log --graph";
       co = "checkout";
     };
     extraConfig = {
